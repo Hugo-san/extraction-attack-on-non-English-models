10c2d09bee5b0807dad55c735c9bffd71e53ee3a
Hugo-san <942454478@qq.com>
2024-03-14 15:22:20 +1100

    Add sliding window and change parameters
    replace the lowercase metric with sliding window metric;
    change the parameters that need to be input, now we need to tell the script which model to load

--- extraction.py
+++ extraction.py
@@ -28,6 +28,34 @@ def calculatePerplexity(sentence, model, tokenizer):
     loss, logits = outputs[:2]
     return torch.exp(loss)
 
+def calcualte_window_perplexity(
+    sentence: str,
+    model,
+    tokenizer,
+    window_size: int = 50,
+    stride: int = 16,
+):
+    ## input_ids == target_ids.
+    input_ids = torch.tensor(tokenizer.encode(sentence)).unsqueeze(0)
+    target_ids = input_ids.clone()
+
+    ppls = []
+    for idx in range(0, input_ids.size(1) - window_size, stride):
+        ## Evaluate on a gpu.
+        with torch.no_grad():
+            outputs = model(
+                input_ids[:, idx : idx + window_size].to(device),
+                labels=target_ids[:, idx : idx + window_size].to(device),
+            )
+
+        ## And the perplexity is exponential of the loss of a sentence.
+        loss, _ = outputs[:2]
+        ppl = float(torch.exp(loss).cpu().detach().numpy())
+        ppls.append(ppl)
+    
+    ## List "ppls" might be empty because of the full punctuations.
+    return np.inf if ppls == [] else min(ppls)
+
 def print_best(metric, samples, name1, scores1, name2=None, scores2=None, n=10):
     """
     print the `n` best samples according to the given `metric`
@@ -57,27 +85,27 @@ def main():
     top_k = 40
 
     print("Loading GPT2...")
-    tokenizer = AutoTokenizer.from_pretrained('uer/gpt2-xlarge-chinese-cluecorpussmall')
+    tokenizer = AutoTokenizer.from_pretrained(args.pretrained-model-name1)
     tokenizer.padding_side = 'left' 
     tokenizer.add_special_tokens({'pad_token': '[PAD]'})
     #tokenizer.pad_token = tokenizer.eos_token
 
-    model1 = AutoModelForCausalLM.from_pretrained('uer/gpt2-xlarge-chinese-cluecorpussmall', return_dict=True).to(device)
+    model1 = AutoModelForCausalLM.from_pretrained(args.pretrained-model-name1, return_dict=True).to(device)
     model1.config.pad_token_id = model1.config.eos_token_id
     model1.resize_token_embeddings(len(tokenizer))
-    model2 = AutoModelForCausalLM.from_pretrained('uer/gpt2-chinese-cluecorpussmall', return_dict=True).to(device)
+    model2 = AutoModelForCausalLM.from_pretrained(args.pretrained-model-name2, return_dict=True).to(device)
     model1.eval()
     model2.eval()
     
     samples = []
-    scores = {"XL": [], "S": [], "Lower": [], "zlib": []}
+    scores = {"XL": [], "S": [], "Sliding_window": [], "zlib": []}
 
-    num_batches = int(np.ceil(args.N / args.batch_size))
+    num_batches = int(np.ceil(args.N / args.batch-size))
     with tqdm(total=args.N) as pbar:
         for i in range(num_batches):
             # encode the prompts
-            #prompts = ["<|endoftext|>"] * args.batch_size
-            prompts = [tokenizer.decode([model1.config.eos_token_id])] * args.batch_size
+            #prompts = ["<|endoftext|>"] * args.batch-size
+            prompts = [tokenizer.decode([model1.config.eos_token_id])] * args.batch-size
             input_len = 1
             inputs = tokenizer(prompts, return_tensors="pt", padding=True)
 
@@ -98,8 +126,8 @@ def main():
                 p1 = calculatePerplexity(text, model1, tokenizer)
                 p2 = calculatePerplexity(text, model2, tokenizer)
 
-                # perplexity on lower-case sample
-                p_lower = calculatePerplexity(text.lower(), model1, tokenizer)
+                # perplexity on sliding window sample
+                p_window = calcualte_window_perplexity(text, model1, tokenizer,args.window_size,args.stride)
 
                 # Zlib "entropy" of sample
                 zlib_entropy = len(zlib.compress(bytes(text, 'utf-8')))
@@ -107,14 +135,14 @@ def main():
                 samples.append(text)
                 scores["XL"].append(p1)
                 scores["S"].append(p2)
-                scores["Lower"].append(p_lower)
+                scores["Sliding_window"].append(p_window)
                 scores["zlib"].append(zlib_entropy)
 
-            pbar.update(args.batch_size)
+            pbar.update(args.batch-size)
 
     scores["XL"] = np.asarray([item.cpu().numpy() for item in scores["XL"]])
     scores["S"] = np.asarray([item.cpu().numpy() for item in scores["S"]])
-    scores["Lower"] = np.asarray([item.cpu().numpy() for item in scores["Lower"]])
+    scores["Sliding_window"] = np.asarray([item.cpu().numpy() for item in scores["Sliding_window"]])
     scores["zlib"] = np.asarray(scores["zlib"])
 
     # Sort by perplexity
@@ -131,10 +159,10 @@ def main():
     print()
     print()
 
-    # Sort by ratio of log perplexities of lower-case and normal-case perplexities 
-    metric = np.log(scores["Lower"]) / np.log(scores["XL"])
-    print(f"======== top sample by ratio of lower-case and normal-case perplexities: ========")
-    print_best(metric, samples, "PPL-XL", scores["XL"], "PPL-XL-Lower", scores["Lower"])
+    # Sort by sliding window perplexities 
+    metric = np.log(scores["Sliding_window"])
+    print(f"======== top sample by sliding window perplexities: ========")
+    print_best(metric, samples, "PPL-XL", scores["XL"], "PPL-XL-Sliding-window", scores["Sliding_window"])
     print()
     print()
 
@@ -147,8 +175,10 @@ def parse_arguments(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('--N', type=int, default=1000, help="Number of samples to generate")
     parser.add_argument('--batch-size', type=int, default=10, help="Batch size for generation")
-    parser.add_argument('--internet-sampling', action='store_true', help="condition the generation using commoncrawl")
-    parser.add_argument('--wet-file', type=str, default=None, help="path to a commoncrawl WET file")
+    parser.add_argument('--pretrained-model-name1', type=str, default=None, help="the target model name of the model from huggingface")
+    parser.add_argument('--pretrained-model-name2', type=str, default=None, help="the name of the model for relative perplexity from huggingface")
+    parser.add_argument('--window_size', type=int, default=50, help="the size of sliding window")
+    parser.add_argument('--stride', type=int, default=16, help="the size of the stride used in sliding window")
     return parser.parse_args(argv)
 
 if __name__ == '__main__':
