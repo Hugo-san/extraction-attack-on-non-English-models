Adding a naive de-duplication; Fix window ppl calculation
Adding a naive de-duplication;
Fix window ppl calculation to remove stride to see if better results

extraction.py
@@ -14,6 +14,7 @@ import torch
 import zlib
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from tqdm import tqdm
+import pandas as pd
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -28,33 +29,19 @@ def calculatePerplexity(sentence, model, tokenizer):
     loss, logits = outputs[:2]
     return torch.exp(loss)
 
-def calcualte_window_perplexity(
-    sentence: str,
-    model,
-    tokenizer,
-    window_size: int = 50,
-    stride: int = 16,
-):
-    ## input_ids == target_ids.
-    input_ids = torch.tensor(tokenizer.encode(sentence)).unsqueeze(0)
-    target_ids = input_ids.clone()
-
-    ppls = []
-    for idx in range(0, input_ids.size(1) - window_size, stride):
-        ## Evaluate on a gpu.
-        with torch.no_grad():
-            outputs = model(
-                input_ids[:, idx : idx + window_size].to(device),
-                labels=target_ids[:, idx : idx + window_size].to(device),
-            )
-
-        ## And the perplexity is exponential of the loss of a sentence.
-        loss, _ = outputs[:2]
-        ppl = float(torch.exp(loss).cpu().detach().numpy())
-        ppls.append(ppl)
-    
-    ## List "ppls" might be empty because of the full punctuations.
-    return np.inf if ppls == [] else min(ppls)
+def calcualte_window_perplexity(input_sentence, model, tokenizer, device, window_size=50):
+    """
+    Calculate min(exp(loss)) over a sliding window
+    """
+    tokenized = tokenizer(input_sentence)
+    input = torch.tensor(tokenized.input_ids).to(device)
+    min_perplexity = 100000
+    with torch.no_grad():
+        for start_idx in range(input.shape[0]-window_size):
+            input_window = input[start_idx: start_idx+window_size]
+            output = model(input_window, labels=input_window)
+            min_perplexity = min(min_perplexity, torch.exp(output.loss))
+    return min_perplexity
 
 def print_best(metric, samples, name1, scores1, name2=None, scores2=None, n=10):
     """
@@ -85,27 +72,27 @@ def main():
     top_k = 40
 
     print("Loading GPT2...")
-    tokenizer = AutoTokenizer.from_pretrained(args.pretrained-model-name1)
+    tokenizer = AutoTokenizer.from_pretrained(args.pretrained_model_name1)
     tokenizer.padding_side = 'left' 
     tokenizer.add_special_tokens({'pad_token': '[PAD]'})
     #tokenizer.pad_token = tokenizer.eos_token
 
-    model1 = AutoModelForCausalLM.from_pretrained(args.pretrained-model-name1, return_dict=True).to(device)
+    model1 = AutoModelForCausalLM.from_pretrained(args.pretrained_model_name1, return_dict=True).to(device)
     model1.config.pad_token_id = model1.config.eos_token_id
     model1.resize_token_embeddings(len(tokenizer))
-    model2 = AutoModelForCausalLM.from_pretrained(args.pretrained-model-name2, return_dict=True).to(device)
+    model2 = AutoModelForCausalLM.from_pretrained(args.pretrained_model_name2, return_dict=True).to(device)
     model1.eval()
     model2.eval()
     
     samples = []
     scores = {"XL": [], "S": [], "Sliding_window": [], "zlib": []}
 
-    num_batches = int(np.ceil(args.N / args.batch-size))
+    num_batches = int(np.ceil(args.N / args.batch_size))
     with tqdm(total=args.N) as pbar:
         for i in range(num_batches):
             # encode the prompts
             #prompts = ["<|endoftext|>"] * args.batch-size
-            prompts = [tokenizer.decode([model1.config.eos_token_id])] * args.batch-size
+            prompts = [tokenizer.decode([model1.config.eos_token_id])] * args.batch_size
             input_len = 1
             inputs = tokenizer(prompts, return_tensors="pt", padding=True)
 
@@ -127,7 +114,7 @@ def main():
                 p2 = calculatePerplexity(text, model2, tokenizer)
 
                 # perplexity on sliding window sample
-                p_window = calcualte_window_perplexity(text, model1, tokenizer,args.window_size,args.stride)
+                p_window = calcualte_window_perplexity(text, model1, tokenizer,args.window_size)
 
                 # Zlib "entropy" of sample
                 zlib_entropy = len(zlib.compress(bytes(text, 'utf-8')))
@@ -138,12 +125,28 @@ def main():
                 scores["Sliding_window"].append(p_window)
                 scores["zlib"].append(zlib_entropy)
 
-            pbar.update(args.batch-size)
+            pbar.update(args.batch_size)
 
     scores["XL"] = np.asarray([item.cpu().numpy() for item in scores["XL"]])
     scores["S"] = np.asarray([item.cpu().numpy() for item in scores["S"]])
-    scores["Sliding_window"] = np.asarray([item.cpu().numpy() for item in scores["Sliding_window"]])
+    scores["Sliding_window"] = np.asarray(scores["Sliding_window"])
     scores["zlib"] = np.asarray(scores["zlib"])
+
+     # a naive de-duplication strategy
+    idxs = pd.Index(samples)
+    idxs_mask = ~(idxs.duplicated())
+    print(idxs_mask)
+    generated_samples_clean = np.asarray(samples)[idxs_mask]
+    generated_samples_clean = generated_samples_clean.tolist()
+
+    scores["XL"] = scores["XL"][idxs_mask]
+    scores["S"] = scores["S"][idxs_mask]
+    scores["zlib"] = scores["zlib"][idxs_mask]
+    scores["Sliding_window"] = scores["Sliding_window"][idxs_mask]
+
+    assert len(generated_samples_clean) == len(scores["XL"])
+    assert len(scores["S"]) == len(scores["XL"])
+    print("Num duplicates:", len(samples) - len(generated_samples_clean))
 
     # Sort by perplexity
     metric = -np.log(scores["XL"])
@@ -160,7 +163,7 @@ def main():
     print()
 
     # Sort by sliding window perplexities 
-    metric = np.log(scores["Sliding_window"])
+    metric = -np.log(scores["Sliding_window"])
     print(f"======== top sample by sliding window perplexities: ========")
     print_best(metric, samples, "PPL-XL", scores["XL"], "PPL-XL-Sliding-window", scores["Sliding_window"])
     print()
@@ -174,9 +177,9 @@ def main():
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('--N', type=int, default=1000, help="Number of samples to generate")
-    parser.add_argument('--batch-size', type=int, default=10, help="Batch size for generation")
-    parser.add_argument('--pretrained-model-name1', type=str, default=None, help="the target model name of the model from huggingface")
-    parser.add_argument('--pretrained-model-name2', type=str, default=None, help="the name of the model for relative perplexity from huggingface")
+    parser.add_argument('--batch_size', type=int, default=10, help="Batch size for generation")
+    parser.add_argument('--pretrained_model_name1', type=str, default=None, help="the target model name of the model from huggingface")
+    parser.add_argument('--pretrained_model_name2', type=str, default=None, help="the name of the model for relative perplexity from huggingface")
     parser.add_argument('--window_size', type=int, default=50, help="the size of sliding window")
     parser.add_argument('--stride', type=int, default=16, help="the size of the stride used in sliding window")
     return parser.parse_args(argv)