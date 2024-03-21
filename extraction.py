e844750cebd1809d788783da6f31b7c02f01f0d3
Hugo-san <942454478@qq.com>
2024-03-21 23:09:30 +1100

    Update for verification part
    Add code for verification.
    But still need experiments to debug.

--- extraction.py
+++ extraction.py
@@ -15,8 +15,16 @@ import zlib
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from tqdm import tqdm
 import pandas as pd
+import json
+from bs4 import BeautifulSoup
+
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+json_data_web = load_data(json_file, 'web_text_zh_train')
+json_data_news = load_data(json_file, 'news2016zh_train')
+json_data_baike = load_data(json_file, 'baike_qa_train')
+print("verification dataset loaded")
 
 def calculatePerplexity(sentence, model, tokenizer):
     """
@@ -34,7 +42,9 @@ def calculate_window_perplexity(input_sentence, model, tokenizer, window_size=50
     target_ids = input_ids.clone()
 
     ppls = []
-    for idx in range(0, input_ids.size(1) - window_size,16):
+    min_ppl_window_text = ""
+    min_ppl = np.inf
+    for idx in range(0, input_ids.size(1) - window_size, 16):
         window_ids = input_ids[:, idx : idx + window_size]
         
         window_text = tokenizer.decode(window_ids[0])
@@ -48,9 +58,15 @@ def calculate_window_perplexity(input_sentence, model, tokenizer, window_size=50
             )
         loss, _ = outputs[:2]
         ppl = float(torch.exp(loss).cpu().detach().numpy())
-        ppls.append(ppl)
 
-    return np.inf if not ppls else min(ppls)
+        if ppl < min_ppl:
+            min_ppl = ppl
+            min_ppl_window_text = window_text
+
+        ppls.append(ppl)
+    # When ppls is empty, return np.inf and a placeholder text indicating no valid window was found.
+    special_text = "[NO VALID WINDOW FOUND]" * (window_size // len("[NO VALID WINDOW FOUND]"))
+    return min_ppl, min_ppl_window_text if ppls else (np.inf, special_text)
 
 def has_repeated_chars(window_text, threshold=0.4):
     from collections import Counter
@@ -65,7 +81,7 @@ def print_best(metric, samples, name1, scores1, name2=None, scores2=None, n=20):
     print the `n` best samples according to the given `metric`
     """
     idxs = np.argsort(metric)[::-1][:n]
-
+    precision_count = 0
     for i, idx in enumerate(idxs):
         if scores2 is not None:
             print(f"{i+1}: {name1}={scores1[idx]:.3f}, {name2}={scores2[idx]:.3f}, score={metric[idx]:.3f}")
@@ -73,12 +89,70 @@ def print_best(metric, samples, name1, scores1, name2=None, scores2=None, n=20):
             print(f"{i+1}: {name1}={scores1[idx]:.3f}, , score={metric[idx]:.3f}")
 
         print()
-        #for line in samples[idx].split("\n"):
-        #    print(f"\t {line.rstrip()}")
         pprint(samples[idx])
+        occ =  verify_samples(samples[idx])
+        print(f"The occurence of this generated sample is :{occ}")
+        if occ > 0:
+            precision_count = precision_count+1
         print()
         print()
-        
+    
+    precision = precision_count/n
+    print(f"The precision by this metric is :{precision:.2f}, out of {n} best samples, {precision_count} of them are memorized.")
+
+def load_data(json_file, data_type):
+
+    data = []
+    with open(json_file, 'r', encoding='utf-8') as file:
+        for line in file:
+            data.append(json.loads(line.strip()))
+    
+    return data
+
+def check_partial_match(output_text, data_type):
+    occurence = 0
+
+    if data_type == 'web_text_zh_train':
+            dataset = json_data_web
+        elif data_type == 'news2016zh_train':
+            dataset = json_data_news
+        elif data_type == 'Bbaike_qa_train':
+            dataset = json_data_baike
+    
+    for item in dataset:
+        merged_text = ''
+        if data_type == 'web_text_zh_train':
+            merged_text = ' '.join([item.get('title', ''), item.get('desc', ''), item.get('topic', ''),item.get('star', '')item.get('content', '')])
+        elif data_type == 'news2016zh_train':
+            merged_text = ' '.join([item.get('title', ''), item.get('content', ''),item.get('source', ''),item.get('time', '')])
+        elif data_type == 'Bbaike_qa_train':
+            merged_text = ' '.join([item.get('category', ''), item.get('title', ''), item.get('desc', ''),item.get('answer', '')])
+
+        output_text_no_space = output_text.replace(" ", "")
+        merged_text_no_space = merged_text.replace(" ", "")
+        merged_text_clean = remove_html_tags_using_bs4(merged_text_no_space)
+
+        if output_text_no_space in merged_text_clean:
+            print(f'Partial match found in {data_type}: {item.get("qid", item.get("news_id", item.get("id", "")))}')
+            occurence = occurence + 1
+    
+    return occurence
+
+def remove_html_tags_using_bs4(html):
+    soup = BeautifulSoup(html, "lxml")
+    
+    text_only = soup.get_text(separator=" ", strip=True)
+    return text_only
+
+def verify_samples(sample):
+    # verification phase:
+    # different types of datasets
+    data_types = ['web_text_zh_train', 'Bbaike_qa_train', 'news2016zh_train']
+    sum_occurence=0
+    for data_type in data_types:
+        sum_occurence = sum_occurence + check_partial_match(sample,data_type)
+    return sum_occurence
+
 def main():
     print(f"using device: {device}")
 
@@ -102,6 +176,7 @@ def main():
     model2.eval()
     
     samples = []
+    window_samples = []
     scores = {"XL": [], "S": [], "Sliding_window": [], "zlib": []}
 
     num_batches = int(np.ceil(args.N / args.batch_size))
@@ -131,12 +206,13 @@ def main():
                 p2 = calculatePerplexity(text, model2, tokenizer)
 
                 # perplexity on sliding window sample
-                p_window = calculate_window_perplexity(text, model1, tokenizer,args.window_size)
+                p_window,window_text = calculate_window_perplexity(text, model1, tokenizer,args.window_size)
 
                 # Zlib "entropy" of sample
                 zlib_entropy = len(zlib.compress(bytes(text, 'utf-8')))
 
                 samples.append(text)
+                window_samples.append(window_text)
                 scores["XL"].append(p1)
                 scores["S"].append(p2)
                 scores["Sliding_window"].append(p_window)
@@ -156,16 +232,23 @@ def main():
     generated_samples_clean = np.asarray(samples)[idxs_mask]
     generated_samples_clean = generated_samples_clean.tolist()
 
+    window_idxs = pd.Index(window_samples)
+    window_idxs_mask = ~(window_idxs.duplicated())
+    print(window_idxs_mask)
+    # Filter out duplicates while preserving the list structure
+    window_generated_samples_clean = [sample for i, sample in enumerate(window_samples) if window_idxs_mask[i]]
+
     scores["XL"] = scores["XL"][idxs_mask]
     scores["S"] = scores["S"][idxs_mask]
     scores["zlib"] = scores["zlib"][idxs_mask]
-    scores["Sliding_window"] = scores["Sliding_window"][idxs_mask]
+    scores["Sliding_window"] = scores["Sliding_window"][window_idxs_mask]
 
     assert len(generated_samples_clean) == len(scores["XL"])
     assert len(scores["S"]) == len(scores["XL"])
-    print("Num duplicates:", len(samples) - len(generated_samples_clean))
+    assert len(window_generated_samples_clean) == len(scores["Sliding_window"])
+    print("Num duplicates:", len(samples) - len(generated_samples_clean) + len(window_samples)- len(window_generated_samples_clean) )
 
- # Sort by perplexity
+    # Sort by perplexity
     metric = -np.log(scores["XL"])
     print(f"======== top sample by XL perplexity: ========")
     print_best(metric, generated_samples_clean, "PPL", scores["XL"])
@@ -182,7 +265,7 @@ def main():
     # Sort by sliding window perplexities 
     metric = -np.log(scores["Sliding_window"])
     print(f"======== top sample by sliding window perplexities: ========")
-    print_best(metric, generated_samples_clean, "PPL-XL", scores["XL"], "PPL-XL-Sliding-window", scores["Sliding_window"])
+    print_best(metric, window_generated_samples_clean, "PPL-XL", scores["XL"], "PPL-XL-Sliding-window", scores["Sliding_window"])
     print()
     print()
 
@@ -199,6 +282,7 @@ def parse_arguments(argv):
     parser.add_argument('--pretrained_model_name2', type=str, default=None, help="the name of the model for relative perplexity from huggingface")
     parser.add_argument('--window_size', type=int, default=50, help="the size of sliding window")
     parser.add_argument('--stride', type=int, default=16, help="the size of the stride used in sliding window")
+    parser.add_argument('--dataset1', type=str, default=None, help="the dataset used for verification")
     return parser.parse_args(argv)
 
 if __name__ == '__main__':
