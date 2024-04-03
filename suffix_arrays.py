b00211192e627036fc7aeb3eadcaa8e71aec38fc
Hugo-san <942454478@qq.com>
2024-04-03 22:25:34 +1100

    Added extraction_only.py
    This code is for the first try on a new language for extraction. Only for observe the extraction format.

--- suffix_arrays.py
+++ suffix_arrays.py
@@ -18,7 +18,7 @@ def merge_text_and_write_to_txt(json_data, data_type, output_file):
         for item in json_data:
             merged_text = ''
             if data_type == 'web_text_zh_train':
-                merged_text = ' '.join([item.get('title', ''), item.get('desc', ''), item.get('topic', ''),str(item.get('star', '')),item.get('content', '')]).strip()
+                merged_text = ' '.join([item.get('title', ''), item.get('desc', ''),item.get('content', '')]).strip()
             elif data_type == 'news2016zh_train':
                 merged_text = ' '.join([item.get('title', ''), item.get('content', ''),item.get('source', ''),str(item.get('time', ''))]).strip()
             elif data_type == 'baike_qa_train':
@@ -34,7 +34,7 @@ data_types = ['web_text_zh_train', 'baike_qa_train', 'news2016zh_train']
 for data_type in data_types:
     json_file = f'{data_type}.json'
     json_data = load_data(json_file, data_type)
-    output_file = f'{data_type}.txt'
+    output_file = f'{data_type}_v2.txt'
     merge_text_and_write_to_txt(json_data, data_type, output_file)
 
 import pysubstringsearch
@@ -42,12 +42,12 @@ import pysubstringsearch
 # creating a new index file
 # if a file with this name is already exists, it will be overwritten
 writer = pysubstringsearch.Writer(
-    index_file_path='output.idx',
+    index_file_path='sum_index_v2.idx',
 )
 
 # adding entries from file lines
 for data_type in data_types:
-    writer.add_entries_from_file_lines(f'{data_type}.txt')
+    writer.add_entries_from_file_lines(f'{data_type}_v2.txt')
 
 # making sure the data is dumped to the file
 writer.finalize()
